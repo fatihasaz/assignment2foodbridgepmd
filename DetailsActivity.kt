2f5968c34dd9a5e47360e333a6aea5e5f18f67aa
fatihasaz <snurfatiha@yahoo.com>
2025-04-28 18:55:58 +0800

    Update DetailsActivity.kt

--- DetailsActivity.kt
+++ DetailsActivity.kt
@@ -5,8 +5,8 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
-import com.firstapp.hungersden.databinding.ActivityDetailsBinding
-import com.firstapp.hungersden.model.CartItems
+import com.firstapp.FoodBridge.databinding.ActivityDetailsBinding
+import com.firstapp.FoodBridge.model.CartItems
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 
